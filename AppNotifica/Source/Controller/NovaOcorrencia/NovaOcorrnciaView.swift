b2b470b1afb0c456a8eafedab3f74764ff22d1b1
annagomes <annagomes0501@gmail.com>
2022-12-06 10:02:07 -0300

    video11
    criação de novos componentes na tela de Nova Ocorrência;

--- AppNotifica/Source/Controller/NovaOcorrencia/NovaOcorrnciaView.swift
+++ AppNotifica/Source/Controller/NovaOcorrencia/NovaOcorrnciaView.swift
@@ -10,58 +10,67 @@ import Foundation
 import UIKit
 
 class NovaOcorrenciaView: ViewDefault {
-
-    var buttonSalvar = ButtonDefault(botao: "SALVAR")
     
+    lazy var imagem: UIImageView = {
+        let view = UIImageView ()
+        view.image = UIImage(named: "imagemCamera")
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    } ()
+    
+    lazy var titleTextField = TextFieldDefault(placeholder: "Título", keyBordType: .default, returnKeyType: .next)
+    
+    lazy var descriptionTextField = TextFieldDefault(placeholder: "Descrição", keyBordType: .default, returnKeyType: .next)
+    
+    lazy var localizationTextField = TextFieldDefault(placeholder: "Localização", keyBordType: .default, returnKeyType: .next)
+    
+    lazy var statusTextField = TextFieldDefault(placeholder: "Status", keyBordType: .default, returnKeyType: .done)
+    
+    lazy var saveButton = ButtonDefault(botao: "Salvar")
     
    override  func setupVisualElements() {
-       super.setupVisualElements()
-       ButtonDefault.delegate = self
-       
-       
-       self.addSubview(buttonSalvar)
-       
-       buttonSalvar.addTarget(self, action: #selector(salvarTap), for: .touchUpInside)
+       self.addSubview(imagem)
+       self.addSubview(titleTextField)
+       self.addSubview(descriptionTextField)
+       self.addSubview(localizationTextField)
+       self.addSubview(statusTextField)
+       self.addSubview(saveButton)
+
        
        NSLayoutConstraint.activate([
-           
-        buttonSalvar.widthAnchor.constraint(equalToConstant: 374),
-        buttonSalvar.heightAnchor.constraint(equalToConstant: 60),
-        buttonSalvar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
-        buttonSalvar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
-           
+        imagem.heightAnchor.constraint(equalToConstant: 200),
+        imagem.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor,constant: 20),
+        imagem.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+        imagem.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+       
+        titleTextField.widthAnchor.constraint(equalToConstant: 374),
+        titleTextField.heightAnchor.constraint(equalToConstant: 60),
+        titleTextField.topAnchor.constraint(equalTo: imagem.bottomAnchor,constant: 20),
+        titleTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+        titleTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+        
+        descriptionTextField.heightAnchor.constraint(equalToConstant: 40),
+        descriptionTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor,constant: 60),
+        descriptionTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        descriptionTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+        
+        localizationTextField.heightAnchor.constraint(equalToConstant: 40),
+        localizationTextField.topAnchor.constraint(equalTo: descriptionTextField.bottomAnchor,constant: 60),
+        localizationTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        localizationTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+        
+        statusTextField.heightAnchor.constraint(equalToConstant: 40),
+        statusTextField.topAnchor.constraint(equalTo: localizationTextField.bottomAnchor,constant: 60),
+        statusTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        statusTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+        
+        saveButton.heightAnchor.constraint(equalToConstant: 40),
+        saveButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor,constant: 60),
+        saveButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        saveButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15)
        ])
 
     }
     
-}
-
-
-
-@objc
-private func salvarTap(){
-    onSalvarTap?()
-}
-
-}
-
-extension LoginView: UITextFieldDelegate {
-    
-    extension RegisterView: UITextFieldDelegate {
-        
-            
-            
-            func buttonShouldReturn(_ textField: UITextField) -> Bool {
-                
-                if textField == buttonSalvar {
-                    self.buttonSalvar.becomeFirstResponder()
-                 
-                } else {
-                    textField.resignFirstResponder()
-                }
-                
-                return true
-            }
-
-}
 }
